5dd815a3d21f3db5dcffc4646ec3700523bd6c30
maxmay94 <maxmay94@gmail.com>
2022-03-04 16:54:54 -0500

    models created

---
+++ models/deckReview.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const deckReviewSchema = new Schema({
+  rating: {
+    type: Number,
+    min: 1,
+    max:5
+  },
+  content: String
+}, {
+  timestamps: true
+})
+
+const DeckReview = mongoose.model('DeckReview', deckReviewSchema)
+
+export {
+  DeckReview
+}
