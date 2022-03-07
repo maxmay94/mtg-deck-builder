5e26d037368d79dca8c767a9abcd45cd956b6a9a
maxmay94 <maxmay94@gmail.com>
2022-03-07 13:08:02 -0500

    search api kind of working

--- controllers/decks.js
+++ controllers/decks.js
@@ -1,9 +1,53 @@
 import { Deck } from '../models/deck.js'
+import fetch from 'node-fetch'
+
+const findCard = 'cards?name='
+// let searchTerm
+// let apiUrl = `https://api.magicthegathering.io/v1/${findCard}${searchTerm}`
+let picUrl
 
 function index(req, res) {
-  console.log('DEFAULT')
+  Deck.find({})
+  .then(decks => {
+    res.render('decks/index', {
+      decks,
+      title: 'Decks'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/decks")
+  })
+}
+
+function newDeck(req, res) {
+  if(res.req.query && res.req.query.search != '') {
+
+    console.log('----------------------------------------')
+    let searchTerm = res.req.query.search
+    let apiUrl = `https://api.magicthegathering.io/v1/${findCard}${searchTerm}`
+    console.log("::: QUERY ::: ", res.req.query.search)
+    console.log(apiUrl)
+    search(apiUrl)
+    console.log('::: picUrl :::', picUrl)
+  }
+
+  res.render('decks/new', {
+    title: 'New Deck',
+    pic: picUrl
+  })
+  console.log('2')
+}
+
+function search(apiUrl) {
+  fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => picUrl = data.cards[0].imageUrl)
+  .catch(error => console.log('::: SEARCH ERROR :::', error))
 }
 
 export {
-  index
+  index,
+  newDeck as new,
+  search
 }
--- routes/decks.js
+++ routes/decks.js
@@ -6,6 +6,16 @@ const router = Router()
 
 //! localhost:3000/decks
 
+// GET - localhost:3000/decks
+router.get('/', decksCtrl.index)
+// GET - localhost:3000/decks/new
+router.get('/new', decksCtrl.new)
+
+////////////////////////
+// GET - localhost:3000/decks/search
+router.get('/new/search', decksCtrl.search)
+////////////////////////
+
 export {
   router
 }
